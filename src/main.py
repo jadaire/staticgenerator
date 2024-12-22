b2f0fc31dba1f4ca3a07d360b420dba6346875ed
dragonetti <dragonetti@gmail.com>
2024-12-22 17:23:07 -0500

    Last Throws

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "http://www.boot.dev")
+
+    print(node)
+
+
+
+if __name__ == "__main__":
+    main()
